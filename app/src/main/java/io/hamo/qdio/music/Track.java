de0851baf2f32dd9a89456be880e909b6e8e45ec
hugocliffordson <cliffordson.hugo@gmail.com>
2018-09-19 13:42:59 +0200

    bug fixes in track. All tests done for TrackTest, AlbumTest, ArtistTest

--- app/src/main/java/io/hamo/qdio/music/Track.java
+++ app/src/main/java/io/hamo/qdio/music/Track.java
@@ -1,5 +1,6 @@
 package io.hamo.qdio.music;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import kaaes.spotify.webapi.android.models.ArtistSimple;
@@ -8,7 +9,7 @@ public class Track implements MusicObject {
 
     private String URI;
     private String albumURI;
-    private List<String> artistURI;
+    private List<String> artistURI = new ArrayList<>();
     private long durationMs;
     private boolean isPlayable;
     private String name;
@@ -21,17 +22,26 @@ public class Track implements MusicObject {
 
     public Track(kaaes.spotify.webapi.android.models.Track track) {
         this.URI = track.uri;
-        this.albumURI = track.album.uri;
+        if (track.album != null) {
+            this.albumURI = track.album.uri;
+        }
         if (track.artists != null) {
             for (ArtistSimple a : track.artists) {
                 this.artistURI.add(a.uri);
             }
         }
         this.durationMs = track.duration_ms;
-        this.isPlayable = track.is_playable;
+        if (track.is_playable != null) {
+            this.isPlayable = track.is_playable;
+        }
         this.name = track.name;
-        this.imageURL = track.album.images != null && track.album.images.size() > 0 ?
+        if (track.album != null) {
+            this.imageURL = track.album.images != null && track.album.images.size() > 0 ?
+                    track.album.images.get(0).url : null;
+        }
+        this.imageURL= track.album != null && track.album.images != null && track.album.images.size() > 0 ?
                 track.album.images.get(0).url : null;
+
     }
 
     public String getAlbumURI() {
