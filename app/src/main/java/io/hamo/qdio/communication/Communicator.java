ae0e26b1c22a6c7f585abae1cd953dc367fe70e8
oggewallgren <oskarwallgren@icloud.com>
2018-09-26 12:10:16 +0200

    Added class IncomingMsgQueue and get for class in ICommunicator

--- app/src/main/java/io/hamo/qdio/communication/Communicator.java
+++ app/src/main/java/io/hamo/qdio/communication/Communicator.java
@@ -1,9 +1,15 @@
 package io.hamo.qdio.communication;
 
+import javax.security.auth.callback.Callback;
+
 import io.hamo.qdio.communication.entity.CommandMessage;
 
 public interface Communicator {
+
     void sendCommand(CommandMessage commandMessage);
+    void onReceiveCommand(Callback callback);
+    IncomingMessageQueue getIncomingMessages();
+
 
 
 }
---
+++ app/src/main/java/io/hamo/qdio/communication/IncomingMessageQueue.java
@@ -0,0 +1,10 @@
+package io.hamo.qdio.communication;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import io.hamo.qdio.communication.entity.CommandMessage;
+
+public class IncomingMessageQueue extends Observable {
+
+}
