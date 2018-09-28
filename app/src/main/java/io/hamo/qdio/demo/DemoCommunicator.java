3ebbb0235fe03705c2faed9abe7687934eac7742
vltmn <melkerveltman@gmail.com>
2018-09-28 16:46:28 +0200

    wired up demoroominitservice with masterroom and slaveroom, implemented democommunicator

--- app/src/main/java/io/hamo/qdio/demo/DemoCommunicator.java
+++ app/src/main/java/io/hamo/qdio/demo/DemoCommunicator.java
@@ -1,6 +1,8 @@
 package io.hamo.qdio.demo;
 
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
 import android.widget.ImageView;
 
 import java.util.Queue;
@@ -9,21 +11,20 @@ import io.hamo.qdio.communication.Communicator;
 import io.hamo.qdio.communication.entity.CommandMessage;
 
 public class DemoCommunicator implements Communicator {
+    private final LiveData<Queue<CommandMessage>> incomingMessages;
 
 
-    public DemoCommunicator() {
+    public DemoCommunicator(LiveData<Queue<CommandMessage>> incomingMessages) {
+        this.incomingMessages = incomingMessages;
     }
 
     @Override
     public LiveData<Queue<CommandMessage>> getIncomingMessages() {
-        
-        return null;
-
-
+        return incomingMessages;
     }
 
     @Override
     public void sendCommand(CommandMessage commandMessage) {
-
+        Log.w(getClass().getSimpleName(), "Command message was sent to democommunicator, message was: " + commandMessage.toString());
     }
 }
--- app/src/main/java/io/hamo/qdio/demo/DemoRoomInitService.java
+++ app/src/main/java/io/hamo/qdio/demo/DemoRoomInitService.java
@@ -10,39 +10,64 @@ import android.util.Log;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+import io.hamo.qdio.communication.Communicator;
 import io.hamo.qdio.communication.JsonUtil;
 import io.hamo.qdio.communication.entity.CommandMessage;
+import io.hamo.qdio.room.MasterRoom;
 import io.hamo.qdio.room.Room;
+import io.hamo.qdio.room.SlaveRoom;
 
+/**
+ * Class for testing the domain w/o dependency on android hardware (nearby communications depends on bluetooth, which doesn't work in emulator)
+ * <p>
+ * This class may receive commands via intents sent with Intent.ACTION_SEND action and Intent.EXTRA_TEXT as a json string containing the command message
+ */
 public class DemoRoomInitService {
+    private static final String DEMO_INTENT_ACTION = "io.hamo.qdio.demo_intent";
+    private static final String DEMO_INTENT_EXTRA_KEY = "DATA";
+
     private final Context context;
+    private final MutableLiveData<Queue<CommandMessage>> incomingMsgQueue;
 
     public DemoRoomInitService(Context context) {
         this.context = context.getApplicationContext();
-        final MutableLiveData<Queue<CommandMessage>> incomingMsgQueue = new MutableLiveData<>();
+        incomingMsgQueue = new MutableLiveData<>();
         incomingMsgQueue.setValue(new ArrayDeque<CommandMessage>());
         BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                String json = intent.getStringExtra(Intent.EXTRA_TEXT);
+                Log.i(getClass().getSimpleName(), "Broadcast received!, data: " + intent.toString());
+                String json = intent.getStringExtra(DEMO_INTENT_EXTRA_KEY);
                 CommandMessage commandMessage = JsonUtil.getInstance().deSerializeMessage(json);
-                Queue<CommandMessage> value = incomingMsgQueue.getValue();
-                value.add(commandMessage);
-                incomingMsgQueue.postValue(value);
+                Log.i(getClass().getSimpleName(), "received commandmessage: " + commandMessage.toString());
+                //Queue<CommandMessage> value = incomingMsgQueue.getValue();
+                //value.add(commandMessage);
+                //incomingMsgQueue.postValue(value);
             }
         };
-        IntentFilter intentFilter = null;
-
-        try {
-            intentFilter = new IntentFilter(Intent.ACTION_SEND, Intent.EXTRA_TEXT);
-        } catch (IntentFilter.MalformedMimeTypeException e) {
-            Log.e(getClass().getSimpleName(), "Could net instantiate intentfilter", e);
-        }
+        IntentFilter intentFilter = new IntentFilter(DEMO_INTENT_ACTION);
         context.registerReceiver(broadcastReceiver, intentFilter);
     }
 
-    public Room createDemoSlaveRoom() {
+    private Communicator getCommunicator() {
+        return new DemoCommunicator(incomingMsgQueue);
+    }
 
-        return null;
+    /**
+     * Creates a slave room with a mock communicator, can receive commandmessages through intents
+     *
+     * @return the room created
+     */
+    public Room createDemoSlaveRoom() {
+        return new SlaveRoom(getCommunicator());
+    }
+
+    /**
+     * Creates a master room with a mock communicator, can receive commandmessages through intents
+     *
+     * @return the room created
+     */
+    public Room createDemoMasterRoom() {
+        return new MasterRoom(getCommunicator());
     }
 }
