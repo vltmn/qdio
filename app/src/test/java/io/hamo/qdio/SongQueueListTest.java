33e1e34970ecbbb5d8ea6ea8934d51a8d103bd58
oggewallgren <oskarwallgren@icloud.com>
2018-10-03 14:20:00 +0200

    done writing tests for SongQueueList with Success

--- app/src/test/java/io/hamo/qdio/SongQueueListTest.java
+++ app/src/test/java/io/hamo/qdio/SongQueueListTest.java
@@ -2,8 +2,12 @@ package io.hamo.qdio;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import io.hamo.qdio.TestUtil.MusicData;
 
+import io.hamo.qdio.music.MusicObject;
 import io.hamo.qdio.music.Track;
 
 import static org.junit.Assert.*;
@@ -23,7 +27,24 @@ public class SongQueueListTest {
     }
 
     @Test
-    public void testPopSong() {
+    public void testPeekSong() {
+        SongQueueList queueList = new SongQueueList();
+        Track t1 = new Track(MusicData.getInstance().getTestTrack());
+        Track t2 = new Track(MusicData.getInstance().getTestTrack());
+        queueList.addSong(t1);
+        queueList.addSong(t2);
+        assertEquals(queueList.peekSong(), t1);
+
+    }
+
+    @Test
+    public void testGetAsList() {
+        SongQueueList queueList = new SongQueueList();
+        Track t1 = new Track(MusicData.getInstance().getTestTrack());
+        Track t2 = new Track(MusicData.getInstance().getTestTrack());
+        queueList.addSong(t1);
+        queueList.addSong(t2);
+        assertEquals(queueList.getAsList(), Arrays.asList(t1, t2));
 
     }
 }
