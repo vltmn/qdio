b887da61511247fc77aae5b021386c7ae163de76
hcliffordson <cliffords.contact@gmail.com>
2018-10-03 15:24:35 +0200

    merged playingstatusfragment

---
+++ app/src/main/java/io/hamo/qdio/view/ImageFetchTask.java
@@ -0,0 +1,25 @@
+package io.hamo.qdio.view;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+public class ImageFetchTask extends AsyncTask<String, Void, Bitmap> {
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+        try {
+            InputStream in = new java.net.URL(urldisplay).openStream();
+            mIcon11 = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return mIcon11;
+    }
+}
