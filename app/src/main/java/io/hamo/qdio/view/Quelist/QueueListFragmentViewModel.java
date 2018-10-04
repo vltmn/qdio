604f9c7ca67213c2233ebf0d8dbadbaf83b70ab9
vltmn <melkerveltman@gmail.com>
2018-10-04 08:43:43 +0200

    Created queuelistfragment for showing the que

---
+++ app/src/main/java/io/hamo/qdio/view/Quelist/QueueListFragmentViewModel.java
@@ -0,0 +1,35 @@
+package io.hamo.qdio.view.Quelist;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.os.Handler;
+
+import java.util.List;
+
+import io.hamo.qdio.music.MusicObject;
+import io.hamo.qdio.music.Track;
+import io.hamo.qdio.room.RoomInstanceHolder;
+
+public class QueueListFragmentViewModel extends ViewModel {
+    private final MutableLiveData<List<Track>> queueList;
+    private Handler handler = new Handler();
+    private static final long UPDATE_DELAY = 50;
+
+    public QueueListFragmentViewModel() {
+        this.queueList = new MutableLiveData<>();
+        handler.post(fetchUpdate);
+    }
+
+    private Runnable fetchUpdate = new Runnable() {
+        @Override
+        public void run() {
+            queueList.postValue(RoomInstanceHolder.getRoomInstance().getQueueList().getAsList());
+            handler.postDelayed(fetchUpdate, UPDATE_DELAY);
+        }
+    };
+
+    public LiveData<List<Track>> getQueueList() {
+        return queueList;
+    }
+}
