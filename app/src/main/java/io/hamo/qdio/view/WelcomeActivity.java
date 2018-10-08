f9ce995149a0ce2b018ccdc8e27be0d7cad66446
oggewallgren <oskarwallgren@icloud.com>
2018-10-08 15:38:12 +0200

    added progress icon for createRoom in WelcomeActivity

--- app/src/main/java/io/hamo/qdio/view/WelcomeActivity.java
+++ app/src/main/java/io/hamo/qdio/view/WelcomeActivity.java
@@ -2,27 +2,39 @@ package io.hamo.qdio.view;
 
 import android.Manifest;
 import android.app.Activity;
+import android.arch.lifecycle.Observer;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import io.hamo.qdio.MainActivity;
 import io.hamo.qdio.R;
+import io.hamo.qdio.playback.ConnectFragment;
+import io.hamo.qdio.playback.PlayerFactory;
 import io.hamo.qdio.room.Room;
 import io.hamo.qdio.room.RoomInstanceHolder;
 import io.hamo.qdio.room.init.RoomCreationService;
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends FragmentActivity {
+    private ProgressBar roomConnectProgress;
+    private LinearLayout buttonGroup;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         checkPermissions();
+        roomConnectProgress = findViewById(R.id.roomConnectProgress);
+        buttonGroup = findViewById(R.id.buttonGroup);
     }
 
     private void checkPermissions() {
@@ -36,13 +48,26 @@ public class WelcomeActivity extends Activity {
         }
     }
 
-
     public void createRoom(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        RoomCreationService roomCreationService = new RoomCreationService(this);
-        Room room = roomCreationService.createRoom(getRandomRoomName());
-        RoomInstanceHolder.setRoomInstance(room);
-        startActivity(intent);
+        buttonGroup.setVisibility(View.INVISIBLE);
+        roomConnectProgress.setVisibility(View.VISIBLE);
+        Fragment connectFragment = new ConnectFragment();
+        getSupportFragmentManager().beginTransaction()
+            .add(connectFragment, ConnectFragment.TAG).commit();
+
+        final Intent intent = new Intent(this, MainActivity.class);
+        final RoomCreationService roomCreationService = new RoomCreationService(this);
+        PlayerFactory.getIsInstantiated().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(@Nullable Boolean aBoolean) {
+                if (aBoolean == null || !aBoolean) {
+                    return;
+                }
+                Room room = roomCreationService.createRoom(getRandomRoomName());
+                RoomInstanceHolder.setRoomInstance(room);
+                startActivity(intent);
+            }
+        });
     }
 
     private static String getRandomRoomName() {
@@ -53,4 +78,6 @@ public class WelcomeActivity extends Activity {
         Intent intent = new Intent(this, RoomDiscoveryActivity.class);
         startActivity(intent);
     }
+
+
 }
